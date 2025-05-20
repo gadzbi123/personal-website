d8915d2909d076fc02b1d0d9af89fe59bf256bd5
gadzbi123 <crg1234@gmail.com>
2025-05-20 17:53:48 +0200

    added upload and init scripts for gcloud

---
+++ init_gcp.sh
@@ -0,0 +1,12 @@
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el9-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+sudo dnf install google-cloud-cli
+gcloud init
+gcloud storage buckets create gs://www.gadzbi.com --location=EUROPE-CENTRAL2
---
+++ upload_gcp.sh
@@ -0,0 +1,7 @@
+gcloud storage rm -r gs://www.gadzbi.com/*
+rm -rf ./public
+hugo
+gsutil -m rsync -r public/ gs://www.gadzbi.com/
+gsutil -m acl ch -r -u AllUsers:R gs://www.gadzbi.com/
+gsutil cors set cors.json gs://www.gadzbi.com
+gsutil web set -m index.html -e 404.html gs://www.gadzbi.com
