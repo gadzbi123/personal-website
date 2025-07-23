f41808006609b0c6f20a254ffa213612def605f2
gadzbi123 <crg1234@gmail.com>
2025-07-23 18:08:08 +0200

    fixed linked in and error page

--- upload_gcp.sh
+++ upload_gcp.sh
@@ -4,4 +4,4 @@ hugo
 gsutil -m rsync -r public/ gs://www.gadzbi.com/
 gsutil -m acl ch -r -u AllUsers:R gs://www.gadzbi.com/
 gsutil cors set cors.json gs://www.gadzbi.com
-gsutil web set -m index.html -e 404.html gs://www.gadzbi.com
+gsutil web set -m index.html -e 404.html gs://www.gadzbi.com/404.html
