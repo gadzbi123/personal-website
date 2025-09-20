e2affb9af8fca11cbe1ff217007ac760bba72043
gadzbi123 <crg1234@gmail.com>
2025-09-20 14:00:52 +0200

    updated upload script

--- upload_gcp.sh
+++ upload_gcp.sh
@@ -1,7 +1,6 @@
-gcloud storage rm -r gs://www.gadzbi.com/*
 rm -rf ./public
 hugo
-gsutil -m rsync -r public/ gs://www.gadzbi.com/
+gsutil -m rsync -r -d public/ gs://www.gadzbi.com/
 gsutil -m acl ch -r -u AllUsers:R gs://www.gadzbi.com/
 gsutil cors set cors.json gs://www.gadzbi.com
-gsutil web set -m index.html -e 404.html gs://www.gadzbi.com/404.html
+gsutil web set -m index.html -e 404.html gs://www.gadzbi.com
